ea4f8695a35926092ef549d03ff1fb4aadd388b8
leighav16 <78450703+leighav16@users.noreply.github.com>
2021-12-04 12:52:30 -0500

    Initial commit

---
+++ final_project.m
@@ -0,0 +1,7 @@
+allison = load('Allison.mat');
+hanna   = load('hannah.mat');
+iris    = load('iris.mat');
+kendra  = load('kendra.mat');
+leigha  = load('Leigha.mat');
+megan   = load('megan.mat');
+
